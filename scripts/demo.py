c74e6e9eee2e8e33b07dae610729e19da5c4a85a
shravani-sajekar <ssajekar@charlotte.edu>
2025-05-01 18:43:57 -0400

    Update demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,69 +1,51 @@
 import os
 import sys
-# os.environ["PYOPENGL_PLATFORM"] = "egl"
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-sys.path.append(os.getcwd())
-
-from transformers import Wav2Vec2Processor
-from glob import glob
-
-import numpy as np
 import json
-import smplx as smpl
+
+import torch
+import torch.nn as nn
+import numpy as np
+from transformers import Wav2Vec2Processor
+from torch.utils import data
+import smplx
+from glob import glob
 
 from nets import *
 from trainer.options import parse_args
 from data_utils import torch_data
 from trainer.config import load_JsonConfig
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils import data
 from data_utils.rotation_conversion import rotation_6d_to_matrix, matrix_to_axis_angle
-from data_utils.lower_body import part2full, pred2poses, poses2pred, poses2poses
+from data_utils.lower_body import part2full
 from visualise.rendering import RenderTool
 
-global device
+# Initialize global device
 device = 'cpu'
 
+
+# Function to initialize models based on name and config
 def init_model(model_name, model_path, args, config):
     if model_name == 's2g_face':
-        generator = s2g_face(
-            args,
-            config,
-        )
+        generator = s2g_face(args, config)
     elif model_name == 's2g_body_vq':
-        generator = s2g_body_vq(
-            args,
-            config,
-        )
+        generator = s2g_body_vq(args, config)
     elif model_name == 's2g_body_pixel':
-        generator = s2g_body_pixel(
-            args,
-            config,
-        )
+        generator = s2g_body_pixel(args, config)
     elif model_name == 's2g_LS3DCG':
-        generator = LS3DCG(
-            args,
-            config,
-        )
+        generator = LS3DCG(args, config)
     else:
         raise NotImplementedError
 
+    # Load model checkpoint
     model_ckpt = torch.load(model_path, map_location=torch.device('cpu'))
-    if model_name == 'smplx_S2G':
-        generator.generator.load_state_dict(model_ckpt['generator']['generator'])
-
-    elif 'generator' in list(model_ckpt.keys()):
+    if 'generator' in model_ckpt.keys():
         generator.load_state_dict(model_ckpt['generator'])
     else:
-        model_ckpt = {'generator': model_ckpt}
         generator.load_state_dict(model_ckpt)
 
     return generator
 
 
+# Initialize the data loader
 def init_dataloader(data_root, speakers, args, config):
     if data_root.endswith('.csv'):
         raise NotImplementedError
@@ -104,14 +86,12 @@ def init_dataloader(data_root, speakers, args, config):
             aud_feat_dim=config.Data.aud.aud_feat_dim,
             feat_method=config.Data.aud.feat_method
         )
+    # Load normalization stats
     if config.Data.pose.normalization:
         norm_stats_fn = os.path.join(os.path.dirname(args.model_path), "norm_stats.npy")
         norm_stats = np.load(norm_stats_fn, allow_pickle=True)
         data_base.data_mean = norm_stats[0]
         data_base.data_std = norm_stats[1]
-    else:
-        norm_stats = None
-
     data_base.get_dataset()
     infer_set = data_base.all_dataset
     infer_loader = data.DataLoader(data_base.all_dataset, batch_size=1, shuffle=False)
@@ -119,6 +99,7 @@ def init_dataloader(data_root, speakers, args, config):
     return infer_set, infer_loader, norm_stats
 
 
+# Function to get vertices from smplx model
 def get_vertices(smplx_model, betas, result_list, exp, require_pose=False):
     vertices_list = []
     poses_list = []
@@ -128,18 +109,21 @@ def get_vertices(smplx_model, betas, result_list, exp, require_pose=False):
         vertices = []
         poses = []
         for j in range(i.shape[0]):
+            print(f"Shape of input data for frame {j}: {i[j].shape}")
+            print(f"Shape of left_hand_pose: {i[j][75:120].unsqueeze_(dim=0).shape}")
+            print(f"Shape of right_hand_pose: {i[j][120:165].unsqueeze_(dim=0).shape}")
             output = smplx_model(betas=betas,
                                  expression=i[j][165:265].unsqueeze_(dim=0) if exp else expression,
-                                 jaw_pose=i[j][0:3].unsqueeze_(dim=0),
-                                 leye_pose=i[j][3:6].unsqueeze_(dim=0),
-                                 reye_pose=i[j][6:9].unsqueeze_(dim=0),
-                                 global_orient=i[j][9:12].unsqueeze_(dim=0),
-                                 body_pose=i[j][12:75].unsqueeze_(dim=0),
-                                 left_hand_pose=i[j][75:120].unsqueeze_(dim=0),
-                                 right_hand_pose=i[j][120:165].unsqueeze_(dim=0),
+                                 jaw_pose=i[j][0:3].unsqueeze_(dim=0).float(),
+                                 leye_pose=i[j][3:6].unsqueeze_(dim=0).float(),
+                                 reye_pose=i[j][6:9].unsqueeze_(dim=0).float(),
+                                 global_orient=i[j][9:12].unsqueeze_(dim=0).float(),
+                                 body_pose=i[j][12:75].unsqueeze_(dim=0).float(),
+                                 left_hand_pose=i[j][75:81].unsqueeze_(dim=0).float(),
+                                 right_hand_pose=i[j][120:126].unsqueeze_(dim=0).float(),
                                  return_verts=True)
+            print(f"Output vertices shape: {output.vertices.shape}")
             vertices.append(output.vertices.detach().cpu().numpy().squeeze())
-            # pose = torch.cat([output.body_pose, output.left_hand_pose, output.right_hand_pose], dim=1)
             pose = output.body_pose
             poses.append(pose.detach().cpu())
         vertices = np.asarray(vertices)
@@ -152,18 +136,17 @@ def get_vertices(smplx_model, betas, result_list, exp, require_pose=False):
         return vertices_list, None
 
 
-global_orient = torch.tensor([3.0747, -0.0158, -0.0152])
-
-
+# Main inference function
 def infer(g_body, g_face, smplx_model, rendertool, config, args):
-    betas = torch.zeros([1, 300], dtype=torch.float64).to(device)
+    betas = torch.zeros([1, 310], dtype=torch.float32).to(device)
     am = Wav2Vec2Processor.from_pretrained("vitouphy/wav2vec2-xls-r-300m-phoneme")
     am_sr = 16000
     num_sample = args.num_sample
-    cur_wav_file = args.audio_file
+    cur_wav_file = '/Users/shravanisajekar/Desktop/CCN/TALKSHOW/demo_audio/1st-page.wav'
     id = args.id
     face = args.only_face
     stand = args.stand
+
     if face:
         body_static = torch.zeros([1, 162], device=device)
         body_static[:, 6:9] = torch.tensor([3.0747, -0.0158, -0.0152]).reshape(1, 3).repeat(body_static.shape[0], 1)
@@ -174,13 +157,11 @@ def infer(g_body, g_face, smplx_model, rendertool, config, args):
                                       initial_pose=None,
                                       norm_stats=None,
                                       w_pre=False,
-                                      # id=id,
                                       frame=None,
                                       am=am,
                                       am_sr=am_sr
                                       )
     pred_face = torch.tensor(pred_face).squeeze().to(device)
-    # pred_face = torch.zeros([gt.shape[0], 105])
 
     if config.Data.pose.convert_to_6d:
         pred_jaw = pred_face[:, :6].reshape(pred_face.shape[0], -1, 6)
@@ -196,7 +177,6 @@ def infer(g_body, g_face, smplx_model, rendertool, config, args):
         pred_res = g_body.infer_on_audio(cur_wav_file,
                                          initial_pose=None,
                                          norm_stats=None,
-                                         txgfile=None,
                                          id=id,
                                          var=None,
                                          fps=30,
@@ -210,9 +190,6 @@ def infer(g_body, g_face, smplx_model, rendertool, config, args):
         else:
             pred = pred[:pred_face.shape[0], :]
 
-        body_or_face = False
-        if pred.shape[1] < 275:
-            body_or_face = True
         if config.Data.pose.convert_to_6d:
             pred = pred.reshape(pred.shape[0], -1, 6)
             pred = matrix_to_axis_angle(rotation_6d_to_matrix(pred))
@@ -223,38 +200,30 @@ def infer(g_body, g_face, smplx_model, rendertool, config, args):
         else:
             pred = torch.cat([pred_jaw, pred, pred_face], dim=-1)
 
-        # pred[:, 9:12] = global_orient
         pred = part2full(pred, stand)
+
         if face:
             pred = torch.cat([pred[:, :3], body_static.repeat(pred.shape[0], 1), pred[:, -100:]], dim=-1)
-        # result_list[0] = poses2pred(result_list[0], stand)
-        # if gt_0 is None:
-        #     gt_0 = gt
-        # pred = pred2poses(pred, gt_0)
-        # result_list[0] = poses2poses(result_list[0], gt_0)
 
         result_list.append(pred)
-
 
     vertices_list, _ = get_vertices(smplx_model, betas, result_list, config.Data.pose.expression)
 
     result_list = [res.to('cpu') for res in result_list]
     dict = np.concatenate(result_list[:], axis=0)
-    file_name = 'visualise/video/' + config.Log.name + '/' + \
-                cur_wav_file.split('\\')[-1].split('.')[-2].split('/')[-1]
+    file_name = 'visualise/video/' + config.Log.name + '/' + cur_wav_file.split('/')[-1].split('.')[-2]
     np.save(file_name, dict)
 
     rendertool._render_sequences(cur_wav_file, vertices_list, stand=stand, face=face, whole_body=args.whole_body)
 
 
+# Main function
 def main():
     parser = parse_args()
     args = parser.parse_args()
-    # device = torch.device(args.gpu)
-    # torch.cuda.set_device(device)
 
-
-    config = load_JsonConfig(args.config_file)
+    config_file_path = '/Users/shravanisajekar/Desktop/CCN/TALKSHOW/config/body_pixel.json'
+    config = load_JsonConfig(config_file_path)
 
     face_model_name = args.face_model_name
     face_model_path = args.face_model_path
@@ -266,34 +235,20 @@ def main():
     os.environ['extra_joint_path'] = config.extra_joint_path
     os.environ['j14_regressor_path'] = config.j14_regressor_path
 
-    print('init model...')
+    print('Initializing model...')
     generator = init_model(body_model_name, body_model_path, args, config)
-    generator2 = None
     generator_face = init_model(face_model_name, face_model_path, args, config)
 
-    print('init smlpx model...')
-    dtype = torch.float64
-    model_params = dict(model_path=smplx_path,
-                        model_type='smplx',
-                        create_global_orient=True,
-                        create_body_pose=True,
-                        create_betas=True,
-                        num_betas=300,
-                        create_left_hand_pose=True,
-                        create_right_hand_pose=True,
-                        use_pca=False,
-                        flat_hand_mean=False,
-                        create_expression=True,
-                        num_expression_coeffs=100,
-                        num_pca_comps=12,
-                        create_jaw_pose=True,
-                        create_leye_pose=True,
-                        create_reye_pose=True,
-                        create_transl=False,
-                        # gender='ne',
-                        dtype=dtype, )
-    smplx_model = smpl.create(**model_params).to(device)
-    print('init rendertool...')
+    print('Initializing SMPLX model...')
+    smplx_model = smplx.create(
+        model_path="/Users/shravanisajekar/Desktop/CCN/TALKSHOW/visualise/smplx/",
+        model_type='smplx',
+        gender='neutral',
+        dtype=torch.float32,
+        num_betas = 310
+    ).to(device)
+
+    print('Initializing render tool...')
     rendertool = RenderTool('visualise/video/' + config.Log.name)
 
     infer(generator, generator_face, smplx_model, rendertool, config, args)
